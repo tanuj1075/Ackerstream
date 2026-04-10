87727de0cf560fadf59626ed96cf2e360f556407
tanuj1075 <tanujdorugade1075@gmail.com>
2026-04-10 19:57:55 +0530

    Remove obsolete _backup folder and its files

--- _backup/backend/routes/showRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getShowsByMovie, getShowById } from '../controllers/showController';
-
-const router = express.Router();
-
-router.get('/movie/:movieId', getShowsByMovie);
-router.get('/:id', getShowById);
-
-export default router;
